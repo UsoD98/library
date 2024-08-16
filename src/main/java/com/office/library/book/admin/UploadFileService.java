8da91631ee684c0e9cf19e94ce7dc0b88b133c95
Jaewon Kim <130049758+UsoD98@users.noreply.github.com>
2024-08-16 17:06:28 +0900

    REFACTOR: 파일 업로드 경로 secret.properties에 은닉

--- src/main/java/com/office/library/book/admin/UploadFileService.java
+++ src/main/java/com/office/library/book/admin/UploadFileService.java
@@ -1,6 +1,7 @@
 package com.office.library.book.admin;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,6 +12,9 @@ import java.util.UUID;
 @Service
 public class UploadFileService {
 
+    @Value("${upload.dir}")
+    private String uploadDir;
+
     public String upload(MultipartFile file) {
 
         boolean result = false;
@@ -18,7 +22,6 @@ public class UploadFileService {
         // 파일 저장
         String fileOriName = file.getOriginalFilename();
         String fileExtension = fileOriName.substring(fileOriName.lastIndexOf("."), fileOriName.length());
-        String uploadDir = "/Users/kjw/MyProject/allinonespring/BookRentalPjt/upload";
 
         UUID uuid = UUID.randomUUID();
         String uniqueName = uuid.toString().replaceAll("-", "");
