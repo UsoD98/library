bbd4e2a806c88138763dc25c9966650cffa794c7
Jaewon Kim <130049758+UsoD98@users.noreply.github.com>
2024-08-16 18:52:14 +0900

    DOCS: 테이블 생성 sql 파일

---
+++ sql/query.sql
@@ -0,0 +1,46 @@
+CREATE TABLE tbl_admin_member(
+                                 a_m_no INT PRIMARY KEY AUTO_INCREMENT,
+                                 a_m_approval INT NOT NULL DEFAULT 0,
+                                 a_m_id VARCHAR(20) NOT NULL,
+                                 a_m_pw VARCHAR(100) NOT NULL,
+                                 a_m_name VARCHAR(20) NOT NULL,
+                                 a_m_gender CHAR(1) NOT NULL,
+                                 a_m_part VARCHAR(20) NOT NULL,
+                                 a_m_position VARCHAR(20) NOT NULL,
+                                 a_m_mail VARCHAR(50) NOT NULL,
+                                 a_m_phone VARCHAR(20) NOT NULL,
+                                 a_m_reg_date DATETIME,
+                                 a_m_mod_date DATETIME
+);
+
+select * from tbl_admin_member;
+
+CREATE TABLE tbl_book(
+                         b_no INT PRIMARY KEY AUTO_INCREMENT,
+                         b_thumbnail VARCHAR(100),
+                         b_name VARCHAR(30) NOT NULL,
+                         b_author VARCHAR(20) NOT NULL,
+                         b_publisher VARCHAR(20) NOT NULL,
+                         b_publish_year CHAR(4) NOT NULL,
+                         b_isbn VARCHAR(30) NOT NULL,
+                         b_call_number VARCHAR(30) NOT NULL,
+                         b_rental_able TINYINT NOT NULL DEFAULT 1,
+                         b_reg_date DATETIME,
+                         b_mod_date DATETIME
+);
+
+select * from tbl_book;
+
+CREATE TABLE tbl_user_member(
+                                u_m_no INT PRIMARY KEY AUTO_INCREMENT,
+                                u_m_id VARCHAR(20) NOT NULL,
+                                u_m_pw VARCHAR(100) NOT NULL,
+                                u_m_name VARCHAR(20) NOT NULL,
+                                u_m_gender CHAR(1) NOT NULL,
+                                u_m_mail VARCHAR(50) NOT NULL,
+                                u_m_phone VARCHAR(20) NOT NULL,
+                                u_m_reg_date DATETIME,
+                                u_m_mod_date DATETIME
+);
+
+select * from tbl_user_member;
