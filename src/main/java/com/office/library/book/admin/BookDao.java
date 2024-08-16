75c2f57aa23883867e3ecf3373e41549cafa3e07
Jaewon Kim <130049758+UsoD98@users.noreply.github.com>
2024-08-16 17:48:19 +0900

    REFACTOR: 도서 정보 수정 UPDATE 쿼리 간소화

--- src/main/java/com/office/library/book/admin/BookDao.java
+++ src/main/java/com/office/library/book/admin/BookDao.java
@@ -135,38 +135,43 @@ public class BookDao {
     public int updateBook(BookVo bookVo) {
         log.info("[BookDao] updateBook HAS BEEN CALLED");
 
+        String sql;
         List<String> args = new ArrayList<>();
 
-        String sql = "UPDATE tbl_book SET ";
+
         if (bookVo.getB_thumbnail() != null) {
-            sql += "b_thumbnail = ?, ";
+            sql = "UPDATE tbl_book SET "
+                    + "b_thumbnail = ?, "
+                    + "b_name = ?, "
+                    + "b_author = ?, "
+                    + "b_publisher = ?, "
+                    + "b_publish_year = ?, "
+                    + "b_isbn = ?, "
+                    + "b_call_number = ?, "
+                    + "b_rental_able = ?, "
+                    + "b_mod_date = NOW() "
+                    + "WHERE b_no = ?";
             args.add(bookVo.getB_thumbnail());
+        } else {
+            sql = "UPDATE tbl_book SET "
+                    + "b_name = ?, "
+                    + "b_author = ?, "
+                    + "b_publisher = ?, "
+                    + "b_publish_year = ?, "
+                    + "b_isbn = ?, "
+                    + "b_call_number = ?, "
+                    + "b_rental_able = ?, "
+                    + "b_mod_date = NOW() "
+                    + "WHERE b_no = ?";
         }
 
-        sql += "b_name = ?, ";
         args.add(bookVo.getB_name());
-
-        sql += "b_author = ?, ";
         args.add(bookVo.getB_author());
-
-        sql += "b_publisher = ?, ";
         args.add(bookVo.getB_publisher());
-
-        sql += "b_publish_year = ?, ";
         args.add(bookVo.getB_publish_year());
-
-        sql += "b_isbn = ?, ";
         args.add(bookVo.getB_isbn());
-
-        sql += "b_call_number = ?, ";
         args.add(bookVo.getB_call_number());
-
-        sql += "b_rental_able = ?, ";
         args.add(Integer.toString(bookVo.getB_rental_able()));
-
-        sql += "b_mod_date = NOW() ";
-
-        sql += "WHERE b_no = ?";
         args.add(Integer.toString(bookVo.getB_no()));
 
         int result = -1;
@@ -174,7 +179,7 @@ public class BookDao {
         try {
             result = jdbcTemplate.update(sql, args.toArray());
         } catch (Exception e) {
-            log.info(e.getMessage());
+            log.error("Error updating book: " + e.getMessage());
         }
 
         return result;
